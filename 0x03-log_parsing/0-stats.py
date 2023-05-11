f06fca157974d6accce4e06d90088c9d8faa8760
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-11 22:10:18 +0300

    Corrected 10 line Condition

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,36 +7,36 @@ import sys
 
 counter = 0
 file_size = 0
-status_code_counter = {200: 0, 301: 0, 400: 0, 401: 0,
-                       403: 0, 404: 0, 405: 0, 500: 0}
+# Store the count of all status_codes in a dictionary
+status_code_counter = {200: 0, 301: 0, 400: 0,
+                       401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
-def print_code_count(dic, file_size):
+def printCodeCount(dic, file_siz):
     """Prints out the status code counts"""
-    print(f"File size: {file_size}")
+    print("File size: {}".format(file_siz))
     for key in sorted(dic.keys()):
-        if dic[key] != 0:
-            print(f"{key}: {dic[key]}")
+        if status_code_counter[key] != 0:
+            print("{}: {}".format(key, dic[key]))
 
 
 if __name__ == "__main__":
     try:
         for line in sys.stdin:
             split_string = re.split('- |"|"| " " ', str(line))
-            status_and_file_s = split_string[-1]
+            statusC_and_file_s = split_string[-1]
             if counter != 0 and counter % 10 == 0:
-                print_code_count(status_code_counter, file_size)
-            counter += 1
+                printCodeCount(status_code_counter, file_size)
+            counter = counter + 1
             try:
-                status_code, f_size = map(int, status_and_file_s.split())
-                if status_code in status_code_counter:
-                    status_code_counter[status_code] += 1
-                file_size += f_size
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                if statusC in status_code_counter:
+                    status_code_counter[statusC] += 1
+                file_size = file_size + f_size
             except ValueError:
                 pass
-            if counter % 10 == 0:
-                print_code_count(status_code_counter, file_size)
-        print_code_count(status_code_counter, file_size)
+        printCodeCount(status_code_counter, file_size)
     except KeyboardInterrupt:
-        print_code_count(status_code_counter, file_size)
+        printCodeCount(status_code_counter, file_size)
         raise
