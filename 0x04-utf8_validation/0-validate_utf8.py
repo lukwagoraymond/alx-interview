1d5780d3869ae39ed5907eb4cc5ff32fa54c0731
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-18 23:49:59 +0300

    Tried new Route

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,7 +8,23 @@ if integer in list is a valid UTF-8 encoding
 def validUTF8(data):
     """UTF-8 Validator Method returns
     True or False"""
-    index = 0
+    state = 0
+    for num in data:
+        if state == 0:
+            if num >> 5 == 0b110:
+                state = 1
+            elif num >> 4 == 0b1110:
+                state = 2
+            elif num >> 3 == 0b11110:
+                state = 3
+            elif num >> 7:
+                return False
+        else:
+            if num >> 6 != 0b10:
+                return False
+            state -= 1
+    return state == 0
+    """index = 0
     encoded_data = iter(data)
     for index in encoded_data:
         leading_ones = count_significant_ones(index)
@@ -20,11 +36,11 @@ def validUTF8(data):
             significant_byte = next(encoded_data, None)
             if significant_byte is None or significant_byte >> 6 != 0b10:
                 return False
-    return True
+    return True"""
 
 
-def count_significant_ones(byte):
-    """Counts the leading one bytes."""
+"""def count_significant_ones(byte):
+    \"""Counts the leading one bytes.\"""
     count = 0
     mask = 0b10000000
 
@@ -32,4 +48,4 @@ def count_significant_ones(byte):
         count += 1
         mask >>= 1
 
-    return count
+    return count"""
