1576df770b9a64fe2d1aff9f5a60c9aa3e34d72d
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-06-28 18:39:55 +0300

    Task Solution Two

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,39 +3,19 @@
 
 
 def isWinner(x, nums):
-    """Function that performs prime game"""
-    if not nums or x < 1:
+    if x < 1 or not nums:
         return None
 
-    def sieve(n, tcase, i=2):
-        if i > int(pow(n, 0.5)) + 1:
-            tcase[0] = tcase[1] = False
-            return
-        if tcase[i]:
-            for j in range(i * i, n + 1, i):
-                tcase[j] = False
-        sieve(n, tcase, i + 1)
-
-    def countPrimes(tcase, i=0, c=0):
-        if i == len(tcase):
-            return
-        if tcase[i]:
-            c += 1
-        tcase[i] = c
-        countPrimes(tcase, i + 1, c)
-
     n = max(nums)
-    tcase = [True for _ in range(max(n + 1, 2))]
-    sieve(n, tcase)
-    countPrimes(tcase)
+    primes = [True] * (n + 1)
+    primes[0] = False
 
-    def calculateScore(nums, player1, i=0):
-        if i == len(nums):
-            if player1 * 2 == len(nums):
-                return None
-            if player1 * 2 > len(nums):
-                return "Maria"
-            return "Ben"
-        player1 += tcase[nums[i]] % 2 == 1
-        return calculateScore(nums, player1, i + 1)
-    return calculateScore(nums, 0)
+    for i in range(2, int(n**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+
+    marias_wins = sum(len([num for num in primes[:n] if num]) % 2 == 1 for n in nums)
+    bens_wins = x - marias_wins
+
+    return 'Maria' if marias_wins < bens_wins else ('Ben' if marias_wins > bens_wins else None)
